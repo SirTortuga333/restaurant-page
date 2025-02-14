9547b287bc9b642d2c2dffab7ca1c91fe11527b7
Gino Fabrizio <51064212+SirTortuga333@users.noreply.github.com>
2025-02-14 15:10:56 -0300

    Made basic home page that works with the button Home

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log("Hello")
+import './styles.css';
+import {homeRender} from './home.js';
+import {menuRender} from './menu.js';
+import {aboutRender} from './about.js';
+
+homeRender();
+
+const contentContainer = document.querySelector('#content');
+
+const homeButton = document.querySelector('#home');
+const menuButton = document.querySelector('#menu');
+const aboutButton = document.querySelector('#about');
+
+
+homeButton.addEventListener('click',(e)=>{
+
+    const toDelete = contentContainer.querySelectorAll('#content > *');
+
+    toDelete.forEach(element => {
+        contentContainer.removeChild(element);
+    });
+
+    homeRender();
+
+})
+
+
+
+menuButton.addEventListener('click',(e)=>{
+
+    const toDelete = contentContainer.querySelectorAll('#content > *');
+
+    toDelete.forEach(element => {
+        contentContainer.removeChild(element);
+    });
+
+    menuRender();
+
+})
+
+
+
+aboutButton.addEventListener('click',(e)=>{
+
+    const toDelete = contentContainer.querySelectorAll('#content > *');
+
+    toDelete.forEach(element => {
+        contentContainer.removeChild(element);
+    });
+
+    aboutRender();
+
+})
